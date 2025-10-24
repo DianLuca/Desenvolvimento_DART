7f3aee98c0b98d4c01f2d30c0da83a62b5f0f43d
DianLuca <dianlucca@hotmail.com>
2025-10-24 19:13:56 -0300

    FEAT: Desenvolvimento da atividade G e desenvolvendo a atividade H

--- Atividade002/Exercicio_G/main.dart
+++ Atividade002/Exercicio_G/main.dart
@@ -1,1 +1,36 @@
 // Sequência de potências:Faça um programa que receba um número inteiro N e imprima as potências de 2 de 0 até N (2⁰, 2¹, ..., 2ᴺ).
+import 'dart:io';
+import 'dart:math';
+void main() {
+  while(true){
+      print('----- Sequência de potências -----');
+      stdout.write('Insira um valor inteiro qualquer para base: ');
+      String? entradaBase = stdin.readLineSync();
+      stdout.write('Insira até que valor inteiro deseja exponenciar: ');
+      String? entradaExpoente = stdin.readLineSync();
+
+      try {
+        int base = int.parse(entradaBase!.trim());
+        int expoente = int.parse(entradaExpoente!.trim());
+        
+        for(int i = 0; i <= expoente; i++){
+          int resultado = pow(base, i).toInt();
+          print('A potência de  base ${base} elevado a ${i} = ${resultado}.');
+        }
+
+        
+        
+      } catch (e) {
+        print('Não foi possível realizar a verificação com os valores inseridos. Tente novamente!');
+      }
+
+
+      stdout.write('\nDeseja encerrar o sistema(s - sim | n - não)? ');
+      String? sair = stdin.readLineSync();
+
+      if(sair != null && sair.trim().isNotEmpty && sair == 's'){
+          print('Programa encerrado!');
+          break;
+      }
+  }
+}
--- Atividade002/Exercicio_H/main.dart
+++ Atividade002/Exercicio_H/main.dart
@@ -1,1 +1,44 @@
 // Cálculo de velocidade média: Faça um programa que receba as distâncias percorridas a cada intervalo de tempo e calcule a velocidade média total do percurso.
+import 'dart:io';
+void main() {
+  while(true){
+      print('----- Cálculo de velocidade média -----');
+      stdout.write('Insira o valor em distância inicial em metros: ');
+      String? entradaDistanciaInicial = stdin.readLineSync();
+      stdout.write('Insira o valor em distância final em metros: ');
+      String? entradaDistanciaFinal = stdin.readLineSync();
+      stdout.write('Insira o valor em tempo inicial em segundos: ');
+      String? entradaTempoInicial = stdin.readLineSync();
+      stdout.write('Insira o valor em tempo final em segundos: ');
+      String? entradaTempoFinal = stdin.readLineSync();
+
+      try {
+        double distanciaInicial = double.parse(entradaDistanciaInicial!);
+        double distanciaFinal = double.parse(entradaDistanciaFinal!);
+        int tempoInicial = int.parse(entradaTempoInicial!);
+        int tempoFinal = int.parse(entradaTempoFinal!);
+
+        double velocidadeMedia;
+
+        // for(int i = tempoInicial; i <= numeroIntervalos; i ++){ // Números de intervalos para somar o total das distâncias e o tempo gasto em cada uma
+          
+        // }
+
+        velocidadeMedia = (distanciaFinal - distanciaInicial)/(tempoFinal - tempoInicial); // Fórmula de velocidade média
+        print('A velocidade média: ${velocidadeMedia}m/s.');
+        
+        
+      } catch (e) {
+        print('Não foi possível realizar a verificação com os valores inseridos. Tente novamente!');
+      }
+
+
+      stdout.write('\nDeseja encerrar o sistema(s - sim | n - não)? ');
+      String? sair = stdin.readLineSync();
+
+      if(sair != null && sair.trim().isNotEmpty && sair == 's'){
+          print('Programa encerrado!');
+          break;
+      }
+  }
+}
