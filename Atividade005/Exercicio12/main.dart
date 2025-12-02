af4563e4a8ad6cdd93863bb27ec6e699063003e5
DianLuca <dianlucca@hotmail.com>
2025-12-02 19:45:49 -0300

    FEAT: Desenvolvendo o exercício 12

--- Atividade005/Exercicio12/main.dart
+++ Atividade005/Exercicio12/main.dart
@@ -4,20 +4,94 @@
 // estão com o status "ativo" igual a true.
 import 'dart:io';
 
+List<Map<String, dynamic>> estoque=[{'nome': 'Pão', 'estoque': 10, 'ativo': true}];
+
 void finalizar() {
   stdout.write('\nDeseja encerrar o sistema(s - sim | n - não)? ');
   String? sair = stdin.readLineSync();
 
-  if(sair != null && sair.trim().isNotEmpty && sair == 's'){
+  if(sair != null && sair.trim().isNotEmpty && sair.toLowerCase() == 's'){
     print('Programa encerrado!');
     exit(0);
-  } else {
-    // palavras.clear();
   }
 }
+
+void exibir(){
+  for (var item in estoque) {
+    print('-' * 70);
+    print('NOME : ${item['nome'].toUpperCase()} - QUANTIDADE(unidade) : ${item['estoque']} - STATUS : ${item['ativo']}');
+  }
+  print('=' * 70);
+}
+
+List adicionar({
+  required String nomeProduto,
+  required int quantidade,
+  required bool status
+}){
+  
+  estoque.add({'nome' : nomeProduto, 'estoque': quantidade, 'ativo': status});
+  print('Produto adicionado com sucesso!');
+  return estoque;
+}
+
 void main(){
   while(true){
-    
-    finalizar();
+    print("\x1B[2J\x1B[0;0H");
+    print('----- Cálculo de Estoque Total de Produtos Ativos -----');
+    stdout.write('1 - CADASTRAR PRODUTO \n2 - EXIBIR LISTA DE PRODUTOS \n3 - ENCERRAR SISTEMA \nSELECIONE A OPÇÃO DESEJADA: ');
+    String? opcao = stdin.readLineSync();
+
+    switch(opcao){
+      case '1':
+        print("\x1B[2J\x1B[0;0H");
+        print('--- ADICIONANDO PRODUTO ---');
+        stdout.write('Insira o nome do produto: ');
+        String? nomeProduto = stdin.readLineSync();
+
+        if(nomeProduto != null && nomeProduto.trim().toUpperCase() != '' ){
+          int quantidade = 0;
+          bool status = false;
+
+          stdout.write('Insira a quantidade de produtos: ');
+          String? entradaQuantidade = stdin.readLineSync();
+
+          if(entradaQuantidade != '' && entradaQuantidade != null){
+            quantidade = int.parse(entradaQuantidade);
+          } else {
+            print('O valor deve ser um inteiro! Valor adotado 0!');
+          }
+
+          stdout.write('O produto está ativo no estoque (s - sim): ');
+          String? entradaStatus = stdin.readLineSync();
+
+          if(entradaStatus != null && entradaStatus.trim().toLowerCase() == 's'){
+            status = true;
+          }
+
+          adicionar(nomeProduto: nomeProduto, quantidade: quantidade, status: status);
+          print('Produto cadastrado com sucesso!');
+        } else {
+          print('\nO nome do produto não pode ser vazio!');
+        }
+        stdout.write('\nPressione "Enter" para voltar ao menu principal: ');
+        stdin.readLineSync();
+        break;
+      case '2':
+        print("\x1B[2J\x1B[0;0H");
+        print('--- EXIBINDO LISTA DE PRODUTOS ---');
+        exibir();
+        stdout.write('\nPressione "Enter" para voltar ao menu principal: ');
+        stdin.readLineSync();
+        break;
+      case '3':
+        finalizar();
+        break;
+      default:
+        stdout.write('Opção não encontrada! Pressione "Enter" para continuar: ');
+        stdin.readLineSync();
+        break;
+    }
+
   }
 }
