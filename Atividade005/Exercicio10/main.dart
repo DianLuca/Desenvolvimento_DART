8059e375b17c8815fb82613dff85d67d3edba590
DianLuca <dianlucca@hotmail.com>
2025-12-01 21:35:50 -0300

    FEAT: Desenvolvendo os exercícios 10 e 11

--- Atividade005/Exercicio10/main.dart
+++ Atividade005/Exercicio10/main.dart
@@ -3,8 +3,7 @@
 // lista original. O uso de List.reversed é permitido para fins de comparação, mas a implementação principal 
 // deve usar uma estrutura de repetição (for ou while).
 import 'dart:io';
-List<String> imperios = ['Império Romano', 'Império Bizantino', 'Império Otomano', 'Império Mongol', 'Império Persa',
- 'Império Inca', 'Império Asteca', 'Império Maurya', 'Império Chines', 'Império Mali'];
+List<String> itens = [];
 
 void finalizar() {
   stdout.write('\nDeseja encerrar o sistema(s - sim | n - não)? ');
@@ -14,17 +13,63 @@ void finalizar() {
     print('Programa encerrado!');
     exit(0);
   } else {
-    // imperios.clear();
+    itens.clear();
   }
 }
+
+List inserirLista({
+  required int vezes
+}){
+  for (int i = 1; i <= vezes; i++){
+    stdout.write('Insira o ${i}º elemento: ');
+    String? entrada = stdin.readLineSync();
+    if(entrada != ''){
+      itens.add(entrada!.trim());
+    } else {
+      print('Você deve inserir algo! Tente novamente.');
+      stdin.readLineSync();
+      i--;
+      print("\x1B[2J\x1B[0;0H");
+    }
+  }
+
+  return itens;
+}
+
+void exibirListaInvertida(){
+  itens = itens.reversed.toList();
+  if (itens.isNotEmpty){
+    for(String item in itens){
+      stdout.write("${item} |");
+    }
+
+    print('\nPressione Enter para continuar.');
+    stdin.readLineSync();
+  } else {
+    print('\nNão há elementos para serem exibidos! Pressione Enter para continuar.');
+    stdin.readLineSync();
+  }
+}
+
 void main() {
   while(true){
-    List<String> listaInversa = imperios.reversed.toList();
-    print(listaInversa);
+    print("\x1B[2J\x1B[0;0H");
+    print('----- Reversão Manual da Ordem dos Elementos -----');
 
-    for(int i = (imperios.length - 1); i >= 0; i--){
-      print(imperios[i]);
-    }
+    stdout.write('Quantos elementos deseja inserir? (mínimo 4): ');
+    String? valEntrada = stdin.readLineSync();
+
+    int? numVezes = int.tryParse(valEntrada!.trim());
+
+    if(numVezes != null && numVezes != '' && numVezes >= 4){
+      inserirLista(vezes: numVezes);
+    } else {
+      print('O mínimo permitido é 4 elementos! Este valor será adotado!');
+      numVezes = 4;
+      inserirLista(vezes: numVezes);
+    } 
+    
+    exibirListaInvertida();
     
     finalizar();
   }
--- Atividade005/Exercicio11/main.dart
+++ Atividade005/Exercicio11/main.dart
@@ -2,12 +2,7 @@
 // Dada uma lista de Maps, onde cada Map representa um usuário com as chaves "nome" (String) e "idade" (int), 
 // filtre e colete em uma nova lista apenas os Maps dos usuários que são maiores de 18 anos.
 import 'dart:io';
-Map<String, int> pessoas = {
-  'João': 25,
-  'Maria': 15,
-  'José': 10,
-  'Bruna': 19,
-};
+List<Map<String, dynamic>> pessoas = [];
 
 void finalizar() {
   stdout.write('\nDeseja encerrar o sistema(s - sim | n - não)? ');
@@ -21,9 +16,53 @@ void finalizar() {
   }
 }
 
+void inserirPessoas(){
+  while(true){
+    print("\x1B[2J\x1B[0;0H");
+    print('----- Seleção de Registros por Idade Mínima -----');
+    print('--- Inserindo -----');
+    stdout.write('Digite o nome: ');
+    String? entradaNome = stdin.readLineSync();
+
+    stdout.write('Digite a idade: ');
+    String? entradaIdade = stdin.readLineSync();
+
+    int idade = int.parse(entradaIdade!.trim());
+
+    pessoas.add({'nome': entradaNome!.trim().toUpperCase(), 'idade': idade});
+
+    stdout.write('Deseja adicionar outra pessoa(s - sim | n - não): ');
+    String? entradaParada = stdin.readLineSync();
+
+    if(entradaParada == 'n' && entradaParada != null){
+      break;
+    }
+  }
+}
+
+void verificarMaiores(){
+  print('\nOs nomes a seguir são de pessoas maiores de 18 anos:');
+  for(dynamic pessoa in pessoas){
+    if(pessoa['idade'] >= 18){
+      stdout.write('Nome: ${pessoa['nome']} - Idade: ${pessoa['idade']} | ');
+    }
+  }
+
+  print('\nOs nomes a seguir são de pessoas menores de 18 anos:');
+  for(dynamic pessoa in pessoas){
+    if(pessoa['idade'] < 18){
+      stdout.write('Nome: ${pessoa['nome']} - Idade: ${pessoa['idade']} | ');
+    }
+  }
+}
+
 void main(){
   while(true){
-        
-        finalizar();
-    }
+
+    inserirPessoas();
+
+    verificarMaiores();
+    
+    finalizar();
+  }
 }
