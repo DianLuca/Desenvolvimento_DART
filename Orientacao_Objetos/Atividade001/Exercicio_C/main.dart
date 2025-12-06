bc1e74b6f3e2853eda489e5f8cf2278677884380
DianLuca <dianlucca@hotmail.com>
2025-12-05 21:51:34 -0300

    FEAT: Desenvolvendo os exercícios C e D

--- Orientacao_Objetos/Atividade001/Exercicio_C/main.dart
+++ Orientacao_Objetos/Atividade001/Exercicio_C/main.dart
@@ -2,7 +2,29 @@ import "../modules/utils.dart";
 import "dart:io";
 
 class Calcular{
-    
+    num nota = 0;
+    List<dynamic> notas = [];
+
+    Calcular();
+
+    void calcularMedia(){
+        num somaNotas = 0;
+
+        for(int nota in notas){
+            somaNotas += nota;
+        }
+
+        double media = somaNotas / notas.length;
+        
+        print('\nA média das ${notas.length} notas adicionadas é: ${media.toStringAsFixed(2)}.');
+        
+        // Após o cálculo a lista deve ser limpa para não ficar 'lixo' para uma próxima operação
+        notas.clear();
+    }
+
+    void adicionarNota(nota){
+        notas.add(nota);
+    }
 }
 
 void main(){
@@ -12,8 +34,27 @@ void main(){
     // Faça um programa que peça 4 notas com  entrada de dados. O programa deverá calcular a média das notas digitadas.
     while(true){
         acoes.limparTela();
+        Calcular calcular = Calcular();
 
-        
+        print('----- Cálculo de Média -----');
+
+        for(int i = 1; i <= 4; i++){
+            stdout.write('Insira a ${i}ª nota: ');
+            String? nota = stdin.readLineSync();
+
+            dynamic verificar = validacoes.verificarNuloVazio(nota);
+            dynamic retorno = validacoes.vericarNumeros(verificar);
+
+
+            if(retorno != false){
+                calcular.adicionarNota(retorno);
+            } else {
+                i--;
+            }
+
+        }
+
+        calcular.calcularMedia();
 
         acoes.finalizar();
     }
--- Orientacao_Objetos/Atividade001/Exercicio_D/main.dart
+++ Orientacao_Objetos/Atividade001/Exercicio_D/main.dart
@@ -1,8 +1,18 @@
 import "../modules/utils.dart";
 import "dart:io";
 
-class Calcular{
-    
+class Formatar{
+    num valor;
+    num valor2;
+
+    Formatar({required this.valor, required this.valor2});
+
+    void divisaoFormatada(){
+        double divisao = valor / valor2;
+        print('$valor ÷ $valor2 = ${divisao.toStringAsFixed(4)}.');
+    }
+
+
 }
 
 void main(){
@@ -12,8 +22,24 @@ void main(){
     // Faça um programa que receba e divida 2 números. A saída da divisão precisará ser formatada com 4 casas decimais.
     while(true){
         acoes.limparTela();
+        print('----- Divisão com 4 casas decimais -----');
 
-        
+        stdout.write('Insira o primeiro valor: ');
+        String? entrada = stdin.readLineSync();
+
+        stdout.write('Insira o segundo valor: ');
+        String? entrada2 = stdin.readLineSync();
+
+        dynamic verificar = validacoes.verificarNuloVazio(entrada);
+        dynamic valorVerificado = validacoes.vericarNumeros(verificar);
+
+        dynamic verificar2 = validacoes.verificarNuloVazio(entrada2);
+        dynamic valorVerificado2 = validacoes.vericarNumeros(verificar2);
+
+        if(valorVerificado != false && valorVerificado2 != false){
+            Formatar formatar = Formatar(valor: valorVerificado, valor2: valorVerificado2);
+            formatar.divisaoFormatada();
+        }
 
         acoes.finalizar();
     }
